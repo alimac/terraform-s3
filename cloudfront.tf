aff4c4ed747ab29e0289849df993f02148fb7531
Alina Mackenzie <helloalimac@gmail.com>
2018-03-08 18:52:25 -0600

    Add CloudFront distributions and DNS records

---
+++ cloudfront.tf
@@ -0,0 +1,103 @@
+# CloudFront distribution - primary domain (S3 origin)
+resource "aws_cloudfront_distribution" "primary_domain" {
+  aliases = ["${var.primary_domain}"]
+  default_root_object = "index.html"
+  enabled = true
+  price_class = "PriceClass_100"
+
+  origin {
+    domain_name = "${aws_s3_bucket.primary_domain.bucket_domain_name}"
+    origin_id = "S3-${var.primary_domain}"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id= "S3-${var.primary_domain}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  tags {
+    Name = "${var.primary_domain}"
+  }
+}
+
+# CloudFront distribution - secondary domain (Custom origin)
+resource "aws_cloudfront_distribution" "secondary_domain" {
+  aliases = ["${var.secondary_domain}"]
+  enabled = true
+  price_class = "PriceClass_100"
+
+  origin {
+    domain_name = "${aws_s3_bucket.secondary_domain.website_endpoint}"
+    origin_id = "S3-${var.secondary_domain}"
+
+    custom_origin_config {
+      http_port = "80"
+      https_port = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id= "S3-${var.secondary_domain}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  tags {
+    Name = "${var.secondary_domain}"
+  }
+}
--- zone.tf
+++ zone.tf
@@ -7,3 +7,29 @@ provider "aws" {
 resource "aws_route53_zone" "zone" {
   name = "${var.primary_domain}."
 }
+
+# Primary domain DNS record
+resource "aws_route53_record" "primary_domain" {
+  zone_id = "${aws_route53_zone.zone.zone_id}"
+  name = "${var.primary_domain}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.primary_domain.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.primary_domain.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+# Secondary domain DNS record
+resource "aws_route53_record" "secondary_domain" {
+  zone_id = "${aws_route53_zone.zone.zone_id}"
+  name = "${var.secondary_domain}"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.secondary_domain.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.secondary_domain.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
